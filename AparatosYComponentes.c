d22b866f62c377d242d01d4c7ee40c96a7a0da4e
Alexiaa-Guerrero <karlaalexia.guerrerogonzalez@gmail.com>
2024-10-25 14:52:35 -0600

    Update AparatosYComponentes.c

--- AparatosYComponentes.c
+++ AparatosYComponentes.c
@@ -1,6 +1,6 @@
 /*Karla Alexia Guerrero González
-Práctica 8 04/10/24
-Tablas de multiplicar*/
+Práctica 11 25/10/24
+Programa  que registra aparatos y sus componentes*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,18 +9,37 @@ void llenar(arr[10][100])
 {
   int i, j, num2
 
-  for(i=0; i<10; i++)
+  for(i = 0; i < 10; i++)
   {
     printf("Dame la cantidad: ");
     scanf("%d", &num);
 
-    for(j=0; j<num; j++)
+    for(j = 0; j < num; j++)
     {
       printf("Dame el número de componente (0-99): ");
       scanf("%d", &num2);
       arr[i][num2] = 1;
     }
   }
-      
 
-  
+
+void mensual(aparmen[10])
+{
+  int i;
+
+  for(i = 0; i < 10; i++)
+  {
+    printf("Número de aparatos mensuales de %d: ", i);
+    scanf("%d", &aparmen[i]);
+  }
+}
+
+
+void comp
+
+
+
+
+
+
+    
